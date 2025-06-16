6fb8eea42658d3f7671da8d03be1946172325d24
hpierrac <h.pierrache@gmail.com>
2025-06-16 10:29:47 +0200

    vérification de mail fonctionnelle

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,8 @@ use App\Http\Controllers\SponsorController;
 // Routes accessibles à tous (login, inscription, etc.)
 require __DIR__.'/auth.php';
 
-// Toutes les autres routes nécessitent une connexion
-Route::middleware('auth')->group(function () {
+// Toutes les autres routes nécessitent une connexion ET un email vérifié
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/home', [EventController::class, 'index'])->name('home');
     Route::resource('events', EventController::class);
     Route::resource('places', PlaceController::class);
